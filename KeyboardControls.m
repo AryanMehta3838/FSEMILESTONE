2a22aa03050c47ebb8ebf816a0645bffacfd8c05
AryanMehta3838 <amehta3838@gmail.com>
2025-03-19 18:24:18 -0700

    Update KeyboardControls.m

--- KeyboardControls.m
+++ KeyboardControls.m
@@ -1,53 +1,36 @@
 global key
 InitKeyboard();
 while 1
-    pause(0.1);
+    pause(.1);
     switch key
         case 'uparrow'
-            moveForward(100,1);
+             brick.MoveMotor('A', 50);
+             brick.MoveMotor('B', 50);
+             pause(1);
+             brick.StopAllMotors();
         case 'downarrow'
-            moveBackward(100,1);
+            brick.MoveMotor('A',-50);
+            brick.MoveMotor('B',-50);
+            pause(1);
+            brick.StopAllMotors();
         case 'rightarrow'
-            turnRight();
+            brick.MoveMotor('A',50);
+            pause(1);
+            brick.StopMotor('A');
         case 'leftarrow'
-            turnLeft();
+            brick.MoveMotor('B',50);
+            pause(1);
+            brick.StopMotor('B');
         case 'q'
+            brick.StopAllMotors();
             break;
         case 0
             disp("Press key to control");
     end
 end
 CloseKeyboard();
-
-function move = moveForward(Speed, Time)
-    brick.MoveMotor('A', Speed);
-    brick.MoveMotor('B', Speed);
-    pause(Time);
-    Stop();
-    move = 1;
-end
-function move = moveBackward(Speed,time)
-    brick.MoveMotor('A',-1*Speed);
-    brick.MoveMotor('B',-1*Speed);
-    pause(time);
-    Stop();
-    move = 1;
-end
-function stop = Stop()
-    brick.StopAllMotors();
-    stop = 1;
-end
-function turn = turnRight()
-    brick.MoveMotor('A',50);
-    pause(0.1);
-    brick.StopMotor('A');
-    turn = 1;
-end
-function turn = turnLeft()
-    brick.MoveMotor('B',50);
-    pause(0.1);
-    brick.StopMotor('B');
-    turn = 1;
-end
+    
+    
 
         
+
