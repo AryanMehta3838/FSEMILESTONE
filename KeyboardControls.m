c344a90408ef6a1491a00b32776039a6d0e9b0bd
AryanMehta3838 <amehta3838@gmail.com>
2025-03-24 17:37:33 -0700

    Update KeyboardControls.m

--- KeyboardControls.m
+++ KeyboardControls.m
@@ -1,36 +1,66 @@
 global key
+% the ultrasonic should be port 1, and the color sensor should be 4
 InitKeyboard();
-while 1
-    pause(.1);
-    switch key
-        case 'uparrow'
-             brick.MoveMotor('A', 50);
-             brick.MoveMotor('B', 50);
-             pause(1);
-             brick.StopAllMotors();
-        case 'downarrow'
-            brick.MoveMotor('A',-50);
-            brick.MoveMotor('B',-50);
-            pause(1);
-            brick.StopAllMotors();
-        case 'rightarrow'
-            brick.MoveMotor('A',50);
-            pause(1);
-            brick.StopMotor('A');
-        case 'leftarrow'
-            brick.MoveMotor('B',50);
-            pause(1);
-            brick.StopMotor('B');
-        case 'q'
-            brick.StopAllMotors();
-            break;
-        case 0
-            disp("Press key to control");
+brick.SetColorMode(4,2);
+function traverse(obj)
+    while 1
+        colorCheck();
+
+        
+
+        
     end
 end
-CloseKeyboard();
+function colorCheck(obj)
+    if brick.ColorCode(4) == 4 || brick.ColorCode(2) == 2 || brick.ColorCode(4) == 3
+        manualMode();
+    end
+    if brick.ColorCode(4) == 5
+        brick.StopAllMotors();
+        pause(1);
+    end
+end
+function manualMode(obj)
+    while 1
+        pause(.1);
+        switch key
+            case 'uparrow'
+                 brick.MoveMotor('A', 50);
+                 brick.MoveMotor('B', 50);
+                 pause(1);
+                 brick.StopAllMotors();
+            case 'downarrow'
+                brick.MoveMotor('A',-50);
+                brick.MoveMotor('B',-50);
+                pause(1);
+                brick.StopAllMotors();
+            case 'rightarrow'
+                brick.MoveMotor('A',50);
+                pause(.1);
+                brick.StopMotor('A');
+            case 'leftarrow'
+                brick.MoveMotor('B',50);
+                pause(.1);
+                brick.StopMotor('B');
+            case 'q'
+                brick.StopAllMotors();
+                break;
+            case 0
+                disp("Press key to control");
+        end
+    end
+    CloseKeyboard();
+    traverse();
+end
+function distance = getDist(obj)
+    distance = brick.UltrasonicDist(1);
+end
+function distArray = Spin(obj)
+    %want this to do a spin and collect the distance to the nearest wall in
+    %each direction, returning an array with the distances: [^,>,back,<]
+end
+
     
     
 
         
-
